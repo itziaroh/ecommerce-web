860ba1d1e42cab60c0da471afccffb674278f414
Itziar Ortega <itziarortega94@gmail.com>
2020-05-27 12:58:54 +0200

    Add graphql and apollo to the addItems functionality

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,8 +1,14 @@
 import { gql } from 'apollo-boost';
+import { addItemToCart } from './cart.utils';
 
 export const typeDefs = gql`
+  extend type Item{
+    quantity: Int
+  }
+
   extend type Mutation {
     ToggleCartHidden: Boolean!
+    AddItemToCart(item:Item!): [Item]!
   }
 `;
 
@@ -10,7 +16,13 @@ const GET_CART_HIDDEN = gql`
   {
     cartHidden @client
   }
-`
+`;
+
+const GET_CART_ITEMS = gql`
+  {
+    cartItems @client
+  }
+`;
 
 export const resolvers = {
   Mutation: {
@@ -25,6 +37,21 @@ export const resolvers = {
       })
 
       return !cartHidden;
+    },
+
+    addItemToCart: (_root, { item }, { cache }) => {
+      const { cartItems } = cache.readQuery({
+        query: GET_CART_ITEMS
+      });
+
+      const newCartItems = addItemToCart(cartItems, item);
+
+      cache.writeQuery({
+        query: GET_CART_ITEMS,
+        data: { cartItems: newCartItems }
+      });
+
+      return newCartItems;
     }
   }
 }
